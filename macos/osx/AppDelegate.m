3a0ee8d195e928e39808efb8c33c9570729e9de3
Teus Benschop <teusjannette@gmail.com>
2016-11-09 11:18:35 +0100

    Minimalist macOS app case
    The case demonstrates and implements how the embedded webview resizes
    together with the main window

---
+++ macos/osx/AppDelegate.m
@@ -0,0 +1,51 @@
+//
+//  AppDelegate.m
+//
+//  Created by Teus Benschop on 28/05/2015.
+//  Copyright (c) 2015 Teus Benschop. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import <WebKit/WebKit.h>
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet WebView *webview;
+@property (weak) IBOutlet NSWindow *window;
+
+@end
+
+
+@implementation AppDelegate
+
+
+- (void) windowDidResize:(NSNotification *) notification
+{
+    NSSize size = self.window.contentView.frame.size;
+    [[self webview] setFrame:CGRectMake(0, 0, size.width, size.height)];
+}
+
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    NSURL *url = [NSURL URLWithString:@"http://localhost:8080"];
+    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
+    [[[self webview] mainFrame] loadRequest:urlRequest];
+    [self.window setContentView:self.webview];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(windowDidResize:) name:NSWindowDidResizeNotification object:self.window];
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification
+{
+    NSLog (@"terminate");
+}
+
+
+-(BOOL) applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)app
+{
+    return YES;
+}
+
+@end
