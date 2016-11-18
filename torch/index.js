5aeef8a902de7f9c37472a7c65831903302e0b4d
Teus Benschop <teusjannette@gmail.com>
2016-11-18 20:45:08 +0100

    Updated cursor highlighter

--- torch/index.js
+++ torch/index.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-		$("body").mousemove(function(e){
-      $(".splash").css('background-position',(e.pageX-25)+'px '+(e.pageY-25)+'px');
-    });
+  $("body").mousemove(function(e){
+    $(".splash").css('background-position',(e.pageX-25)+'px '+(e.pageY-25)+'px');
+    $(".splash2").css('left',(e.pageX-60)+'px');
+    $(".splash2").css('top',(e.pageY-65)+'px');
+  });
 });
