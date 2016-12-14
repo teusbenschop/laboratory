ca3894b30c47943fa07f95f3d40581136bbc2af4
Teus Benschop <teusjannette@gmail.com>
2016-12-14 14:45:12 +0100

    Remove duplicate code

--- chrome-native/background.js
+++
@@ -1,8 +0,0 @@
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  console.log ("Requesting a wake lock");
-  chrome.power.requestKeepAwake ("system");
-  chrome.power.requestKeepAwake ("display");
-  var url = "http://bibledit.org:8080";
-  chrome.tabs.create ({ url: url });
-});
