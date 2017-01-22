4b0df00611f041be86587b0b8d97ab0270307adc
Teus Benschop <teusjannette@gmail.com>
2017-01-22 14:46:07 +0100

    Undo and redo
    https://github.com/bibledit/bibledit/issues/463

--- libwebkit2gtk/bibledit.cpp
+++ libwebkit2gtk/bibledit.cpp
@@ -1,14 +1,19 @@
-#include <bibledit.h>
-// #include <executable/bibledit.h>
+#include <cstdlib>
 #include <libgen.h>
 #include <iostream>
-// #include "library/bibledit.h"
+#include <gtk/gtk.h>
 #include <webkit2/webkit2.h>
+
+
+using namespace std;
+
+
+static void activate (GtkApplication *app);
 
 
 int main (int argc, char *argv[])
 {
-  application = gtk_application_new ("org.bibledit.linux", G_APPLICATION_FLAGS_NONE);
+  GtkApplication *application = gtk_application_new ("org.bibledit.linux", G_APPLICATION_FLAGS_NONE);
   
   g_signal_connect (application, "activate", G_CALLBACK (activate), NULL);
   
@@ -17,27 +22,48 @@ int main (int argc, char *argv[])
   if (readlink ("/proc/self/exe", linkname, 256)) {};
   string webroot = dirname (linkname);
   free (linkname);
-  //bibledit_initialize_library (webroot.c_str(), webroot.c_str());
   
-  //bibledit_start_library ();
-  
-  status = g_application_run (G_APPLICATION (application), argc, argv);
+  gint status = g_application_run (G_APPLICATION (application), argc, argv);
   
   g_object_unref (application);
-  
-  //bibledit_stop_library ();
-  
-  //while (bibledit_is_running ()) { };
-  
-  //bibledit_shutdown_library ();
   
   return status;
 }
 
 
-void activate (GtkApplication *app)
+gboolean on_key_press (GtkWidget *widget, GdkEvent *event, gpointer data)
 {
-  GList *list = gtk_application_get_windows (app);
+  if (event->type == GDK_KEY_PRESS) {
+    GdkEventKey * event_key = (GdkEventKey *) event;
+    if ((event_key->keyval == GDK_KEY_z) || (event_key->keyval == GDK_KEY_Z)  ) {
+      if (event_key->state & GDK_CONTROL_MASK) {
+        const gchar *command;
+        if (event_key->state & GDK_SHIFT_MASK) {
+          command = WEBKIT_EDITING_COMMAND_REDO;
+        } else {
+          command = WEBKIT_EDITING_COMMAND_UNDO;
+        }
+        WebKitWebView * web_view = WEBKIT_WEB_VIEW (widget);
+        webkit_web_view_execute_editing_command (web_view, command);
+        return true;
+      }
+    }
+  }
+  (void) data;
+  return false;
+}
+
+
+void on_signal_destroy (gpointer user_data)
+{
+  (void) user_data;
+  gtk_main_quit ();
+}
+
+
+void activate (GtkApplication * application)
+{
+  GList *list = gtk_application_get_windows (application);
   
   if (list) {
     gtk_window_present (GTK_WINDOW (list->data));
@@ -45,16 +71,11 @@ void activate (GtkApplication *app)
   }
   
   // The top-level window.
-  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  GtkWidget * window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
   gtk_window_set_title (GTK_WINDOW (window), "Bibledit");
   gtk_window_set_default_size (GTK_WINDOW (window), 800, 600);
   gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_CENTER);
   gtk_window_set_application (GTK_WINDOW (window), application);
-  
-  // The icon.
-  gchar * iconfile = g_build_filename ("bibledit.xpm", NULL);
-  gtk_window_set_default_icon_from_file (iconfile, NULL);
-  g_free (iconfile);
   
   // Prepare for program quit.
   g_signal_connect (window, "destroy", G_CALLBACK (on_signal_destroy), NULL);
@@ -62,11 +83,14 @@ void activate (GtkApplication *app)
   // Create a browser instance.
   WebKitWebView * webview = WEBKIT_WEB_VIEW (webkit_web_view_new ());
   
+  // Signal handlers.
+  g_signal_connect (webview, "key-press-event", G_CALLBACK (on_key_press), NULL);
+  
   // Put the browser area into the main window.
   gtk_container_add (GTK_CONTAINER (window), GTK_WIDGET (webview));
   
   // Load a web page into the browser instance
-  webkit_web_view_load_uri (webview, "http://localhost:8080");
+  webkit_web_view_load_uri (webview, "http://bibledit.org:8080");
  
   // Ensure it will get mouse and keyboard events.
   gtk_widget_grab_focus (GTK_WIDGET (webview));
@@ -76,11 +100,4 @@ void activate (GtkApplication *app)
   
   // Run the main GTK+ event loop.
   gtk_main();
-}
-
-
-void on_signal_destroy (gpointer user_data)
-{
-  (void) user_data;
-  gtk_main_quit ();
 }
