40671481ce33e83a5023aedf7f4096db990f45c4
Teus Benschop <teusjannette@gmail.com>
2017-03-14 10:30:12 +0100

    Write to Console
    https://github.com/bibledit/bibledit/issues/488

--- dot.NET/gui/bibledit/Form1.cs
+++ dot.NET/gui/bibledit/Form1.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Windows.Forms;
-using System.IO;
-using System.IO.IsolatedStorage;
-
+using System.Diagnostics;
 
 namespace Bibledit
 {
@@ -10,30 +8,16 @@ namespace Bibledit
   public partial class Form1 : Form
   {
 
-
     public Form1()
     {
       InitializeComponent();
-    }
-
-
-    private void Form1_Load(object sender, EventArgs e)
-    {
-    }
-
-
-
-
-    private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-    {
+      Console.WriteLine("Hello World.");
     }
 
     private void Form1_KeyDown_1(object sender, KeyEventArgs e)
     {
       if ((e.Control) && (e.KeyCode == Keys.F))
       {
-        Console.WriteLine("Ctrl-F");
-
         System.Drawing.Size size = new System.Drawing.Size(200, 70);
         Form inputBox = new Form();
 
@@ -44,12 +28,11 @@ namespace Bibledit
         System.Windows.Forms.TextBox textBox = new TextBox();
         textBox.Size = new System.Drawing.Size(size.Width - 10, 23);
         textBox.Location = new System.Drawing.Point(5, 5);
-        textBox.Text = "search for, guys!";
+        //textBox.Text = "search for, guys!";
         inputBox.Controls.Add(textBox);
 
         Button okButton = new Button();
         okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-        okButton.Name = "okButton";
         okButton.Size = new System.Drawing.Size(75, 23);
         okButton.Text = "&OK";
         okButton.Location = new System.Drawing.Point(size.Width - 80 - 80, 39);
@@ -57,7 +40,6 @@ namespace Bibledit
 
         Button cancelButton = new Button();
         cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-        cancelButton.Name = "cancelButton";
         cancelButton.Size = new System.Drawing.Size(75, 23);
         cancelButton.Text = "&Cancel";
         cancelButton.Location = new System.Drawing.Point(size.Width - 80, 39);
@@ -73,7 +55,6 @@ namespace Bibledit
         inputBox.StartPosition = FormStartPosition.CenterParent;
         DialogResult result = inputBox.ShowDialog();
         Console.WriteLine (textBox.Text);
-
       }
     }
   }
