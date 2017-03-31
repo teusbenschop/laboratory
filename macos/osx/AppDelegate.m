ceab29808e7406d49dfc91ff333d70b5e7fe5cb8
Teus Benschop <teusjannette@gmail.com>
2017-03-31 18:25:26 +0200

    Cleanup

--- macos/osx/AppDelegate.m
+++ macos/osx/AppDelegate.m
@@ -40,10 +40,6 @@
     [self.window setContentView:self.webview];
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(windowDidResize:) name:NSWindowDidResizeNotification object:self.window];
-    
-    [self.webview setPolicyDelegate:self];
-    [self.webview setDownloadDelegate:self];
-
 }
 
 
@@ -64,48 +60,6 @@
     NSString * url = [[[[frame dataSource] request] URL] absoluteString];
     NSLog (@"%@", url);
     [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString: url]];
-}
-
-
-- (void) webView:(WebView *)webView decidePolicyForMIMEType:(NSString *)type request:(NSURLRequest *)request frame:(WebFrame *)frame decisionListener:(id < WebPolicyDecisionListener >)listener
-{
-    if (![[webView class] canShowMIMEType:type]) [listener download];
-}
-
-
-- (void)download:(NSURLDownload *)download decideDestinationWithSuggestedFilename:(NSString *)filename
-{
-    NSString * directory = @"/tmp";
-    struct passwd *pw = getpwuid(getuid());
-    if (pw->pw_dir) directory = [NSString stringWithUTF8String:pw->pw_dir];
-    if (filename == nil) filename = @"bibledit-download";
-    NSString *destinationPath = [NSString stringWithFormat:@"%@/%@/%@", directory, @"Downloads", filename];
-    NSAlert *alert = [[NSAlert alloc] init];
-    alert.messageText = @"Download";
-    [alert addButtonWithTitle:@"OK"];
-    alert.informativeText = [NSString stringWithFormat:@"Will be saved to: %@", destinationPath];
-    [alert runModal];
-    [download setDestination:destinationPath allowOverwrite:YES];
-}
-
-
-- (void)webView:(WebView *)sender runOpenPanelForFileButtonWithResultListener:(id < WebOpenPanelResultListener >)resultListener
-{
-    // Create the File Open Dialog class.
-    NSOpenPanel* openDlg = [NSOpenPanel openPanel];
-    
-    // Enable the selection of files in the dialog.
-    [openDlg setCanChooseFiles:YES];
-    
-    // Enable the selection of directories in the dialog.
-    [openDlg setCanChooseDirectories:NO];
-    
-    
-    if ( [openDlg runModal] == NSModalResponseOK )
-    {
-        NSArray* files = [[openDlg URLs]valueForKey:@"relativePath"];
-        [resultListener chooseFilenames:files];
-    }
 }
 
 
