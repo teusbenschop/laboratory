768ef4a38352583f36a3c298fb41b532fbcf0d66
Teus Benschop <teusjannette@gmail.com>
2017-04-21 11:13:43 +0200

    Cmd-F and Cmd-G search
    https://github.com/bibledit/bibledit/issues/536

--- macos/osx/AppDelegate.m
+++ macos/osx/AppDelegate.m
@@ -16,24 +16,53 @@
 @implementation AppDelegate
 
 
+NSString * searchText = @"";
+
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     NSURL *url = [NSURL URLWithString:@"http://bibledit.org:8080"];
     NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
     [[[self webview] mainFrame] loadRequest:urlRequest];
     [self.window setContentView:self.webview];
-}
-
-
-- (void)applicationWillTerminate:(NSNotification *)aNotification
-{
-    NSLog (@"terminate");
-}
-
-
--(BOOL) applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)app
-{
-    return YES;
+    [NSEvent addLocalMonitorForEventsMatchingMask:NSEventMaskKeyDown handler:^(NSEvent *incomingEvent) {
+        NSEvent *result = incomingEvent;
+        NSWindow *targetWindowForEvent = incomingEvent.window;
+        if (targetWindowForEvent == self.window) {
+            if (incomingEvent.type == NSEventTypeKeyDown) {
+                NSEventModifierFlags modifier = incomingEvent.modifierFlags;
+                if (modifier & NSEventModifierFlagCommand) {
+                    NSString *characters = incomingEvent.characters;
+                    if ([characters isEqualToString:@"f"]) {
+                        NSAlert *alert = [[NSAlert alloc] init];
+                        alert.messageText = @"Search";
+                        [alert addButtonWithTitle:@"OK"];
+                        [alert addButtonWithTitle:@"Cancel"];
+                        alert.informativeText = [NSString stringWithFormat:@"Search for"];
+                        NSTextField *input = [[NSTextField alloc] initWithFrame:NSMakeRect(0, 0, 200, 24)];
+                        [input setStringValue:searchText];
+                        [alert setAccessoryView:input];
+                        [[alert window] setInitialFirstResponder: input];
+                        NSInteger button = [alert runModal];
+                        if (button == NSAlertFirstButtonReturn) {
+                            searchText = [input stringValue];
+                            if (searchText.length) {
+                                [self.webview searchFor:searchText direction:TRUE caseSensitive:FALSE wrap:TRUE];
+                            } else {
+                                [self.webview setSelectedDOMRange:nil affinity:NSSelectionAffinityDownstream];
+                            }
+                        }
+                        result = nil;
+                    }
+                    if ([characters isEqualToString:@"g"]) {
+                        [self.webview searchFor:searchText direction:TRUE caseSensitive:FALSE wrap:TRUE];
+                        result = nil;
+                    }
+                }
+            }
+        }
+        return result;
+    }];
 }
 
 
