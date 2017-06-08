709876bbc046138767a6c9fdc4140b04b5aeac1f
Teus Benschop <teusjannette@gmail.com>
2017-06-08 20:44:13 +0100

    HelloWorld Universal Windows App
    https://github.com/bibledit/cloud/issues/7

---
+++ uwp/HelloWorld/HelloWorld/MainPage.xaml.cpp
@@ -0,0 +1,45 @@
+﻿//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace HelloWorld;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+}
+
+
+void HelloWorld::MainPage::TextBlock_SelectionChanged (Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+
+}
+
+
+void HelloWorld::MainPage::nameInput_TextChanged (Platform::Object^ sender, Windows::UI::Xaml::Controls::TextChangedEventArgs^ e)
+{
+
+}
+
+
+void HelloWorld::MainPage::inputButton_Click (Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	greetingOutput->Text = "Hello, " + nameInput->Text + "!";
+}
