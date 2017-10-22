bc8c4df239e47af73b2fd71bfacb95d03fcc9815
Teus Benschop <teusjannette@gmail.com>
2017-10-22 16:30:25 +0200

    Simple WebSocket library tests

---
+++ simple-websocket-server/bitfinex.cpp
@@ -0,0 +1,47 @@
+#include "client_wss.hpp"
+
+using namespace std;
+
+int main() {
+
+  using WssClient = SimpleWeb::SocketClient<SimpleWeb::WSS>;
+
+  // Second Client() parameter is for whether to do certificate verification.
+  WssClient client("api.bitfinex.com/ws/2", true);
+
+  client.on_message = [](shared_ptr<WssClient::Connection> connection, shared_ptr<WssClient::Message> message)
+  {
+    auto message_str = message->string();
+    
+    cout << "Client: Message received: \"" << message_str << "\"" << endl;
+    
+    cout << "Client: Sending close connection" << endl;
+    connection->send_close(1000);
+  };
+  
+  client.on_open = [](shared_ptr<WssClient::Connection> connection)
+  {
+    cout << "Client: Opened connection" << endl;
+    
+    string message = "Hello";
+    cout << "Client: Sending message: \"" << message << "\"" << endl;
+    
+    auto send_stream = make_shared<WssClient::SendStream>();
+    *send_stream << message;
+    connection->send(send_stream);
+  };
+  
+  client.on_close = [](shared_ptr<WssClient::Connection>, int status, const string &)
+  {
+    cout << "Client: Closed connection with status code " << status << endl;
+  };
+  
+  // See http://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/reference.html, Error Codes for error code meanings
+  client.on_error = [](shared_ptr<WssClient::Connection> /*connection*/, const SimpleWeb::error_code &ec)
+  {
+    cout << "Client: Error: " << ec << ", error message: " << ec.message() << endl;
+  };
+  
+  client.start();
+}
+
