69f5dd386b3941c94b8088ea2c3e041166e6594d
Teus Benschop <teusjannette@gmail.com>
2019-04-15 14:06:53 +0200

    Python web app sample

---
+++ python/flask-hello.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# https://pythonspot.com/flask-hello-world/
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+
+def hello():
+  return "Hello World!"
+
+if __name__ == "__main__":
+  app.run()
+
