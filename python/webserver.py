5fbbb89183972ada30244e949b6b6ec6c28988e8
Teus Benschop <teusjannette@gmail.com>
2019-04-17 16:07:00 +0200

    Basic webserver in Python

---
+++ python/webserver.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import http.server
+import socketserver
+
+PORT = 8080
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+  print("serving at port", PORT)
+  httpd.serve_forever()
+
+
