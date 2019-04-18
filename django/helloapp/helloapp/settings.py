5562636cfa65deaff0e1294b90daddc898500e9f
Teus Benschop <teusjannette@gmail.com>
2019-04-18 11:37:30 +0200

    Update Allowed_Hosts...
    .... for running on Linux server

--- django/helloapp/helloapp/settings.py
+++ django/helloapp/helloapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z*+))v24sukujdsdp^=ta)qgto$e$n@$h-wnpszaocywoq=(z$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.2.15"]
 
 
 # Application definition
