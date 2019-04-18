40ba9cd96a6faa5f64bb9264e99929ecbc5739f3
Teus Benschop <teusjannette@gmail.com>
2019-04-18 10:50:51 +0200

    Fetch temperatures...
    ... at Amsterdam and store it in the database.

--- django/helloapp/collector/run.py
+++ django/helloapp/collector/run.py
@@ -1,4 +1,8 @@
 import psycopg2
+import time
+import datetime
+from urllib.request import urlopen
+import json
 
 try:
   connection = psycopg2.connect (user = "postgres_user",
@@ -7,15 +11,41 @@ try:
                                  port = "5432",
                                  database = "my_postgres_db")
 
-  print ( connection.get_dsn_parameters())
+  print (connection.get_dsn_parameters())
 
   cursor = connection.cursor()
   cursor.execute("SELECT version();")
   record = cursor.fetchone()
   print (record)
 
+  while (True):
+    
+    try:
+
+      response = urlopen ("http://weerlive.nl/api/json-data-10min.php?key=demo&locatie=Amsterdam")
+      # print (response)
+      data = response.read().decode("utf-8")
+      json_object = json.loads(data)
+      liveweather = json_object ['liveweer'];
+      # print (liveweather)
+      location = liveweather [0]['plaats'];
+      temperature = liveweather [0]['temp'];
+      print (location, temperature, "°C")
+      
+      insert_query = """ INSERT INTO weather (location, temperature, stamp) VALUES (%s, %s, NOW())"""
+      record_to_insert = (location, temperature)
+      cursor.execute (insert_query, record_to_insert)
+      connection.commit()
+      count = cursor.rowcount
+      print (datetime.datetime.now(), count, "record inserted successfully")
+
+    except (Exception) as error:
+      print (error)
+    
+    time.sleep (600)
+
 except (Exception, psycopg2.Error) as error :
-  print ("Error while connecting to PostgreSQL", error)
+  print (error)
 finally:
   if (connection):
     cursor.close()
