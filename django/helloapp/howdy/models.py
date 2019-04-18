68e736c0538bcf7599be1c115a3ed9747c5a5900
Teus Benschop <teusjannette@gmail.com>
2019-04-18 12:24:23 +0200

    Shows most recent temperature

--- django/helloapp/howdy/models.py
+++ django/helloapp/howdy/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+import psycopg2
+
+def get_current_temperature ():
+
+  connection = psycopg2.connect (user = "postgres_user",
+                                 password = "postgres_pass",
+                                 host = "127.0.0.1",
+                                 port = "5432",
+                                 database = "my_postgres_db")
+  cursor = connection.cursor()
+  query = "SELECT temperature FROM weather ORDER BY id DESC LIMIT 1;"
+  cursor.execute (query)
+  record = cursor.fetchone()
+  temperature = record[0]
+
+  if (connection):
+    cursor.close()
+    connection.close()
+  return temperature
+
+
--- django/helloapp/howdy/views.py
+++ django/helloapp/howdy/views.py
@@ -3,11 +3,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 import logging
+import howdy.models
 
 # Create your views here.
 class HomePageView(TemplateView):
   def get(self, request, **kwargs):
-    return render(request, 'index.html', context=None)
+    temperature = howdy.models.get_current_temperature ()
+    return render(request, 'index.html', context = {'temp': temperature})
 
 class AboutPageView(TemplateView):
   template_name = "about.html"
