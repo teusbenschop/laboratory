74821d463d429c2995fc970da629b88d6dfe98f6
Teus Benschop <teusjannette@gmail.com>
2019-05-05 15:14:02 +0200

    Updated storyboard

--- swift/FoodTracker/FoodTracker/ViewController.swift
+++ swift/FoodTracker/FoodTracker/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
     // Do any additional setup after loading the view.
   }
 
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+
 
 }
 
