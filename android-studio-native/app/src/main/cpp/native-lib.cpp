0162a9a0c87ffd24d554a57fa411659f3146f09e
Teus Benschop <teusjannette@gmail.com>
2019-07-25 19:30:31 +0200

    Link with native (C++) code
    and do calls to C++ from Java or Kotlin.

--- android-studio-native/app/src/main/cpp/native-lib.cpp
+++ android-studio-native/app/src/main/cpp/native-lib.cpp
@@ -4,6 +4,6 @@
 extern "C" JNIEXPORT jstring JNICALL
 Java_org_bibleconsultants_lab_MainActivity_stringFromJNI(JNIEnv *env, jobject /* this */)
 {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello from C++ within Bible Consultants Laboratory!";
     return env->NewStringUTF(hello.c_str());
 }
