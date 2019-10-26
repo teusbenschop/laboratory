36df5154f0d54de4551891fa1f4470365546aed8
Teus Benschop <teusjannette@gmail.com>
2019-10-26 19:03:29 +0200

    SwiftUI laboratory project macOS

---
+++ swiftui/Laboratory/Laboratory/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Laboratory
+//
+//  Created by Teus Benschop on 26/10/2019.
+//  Copyright © 2019 Free Software Consultants. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
