4225e3bea11906a90d98203b09c56d704817e621
Teus Benschop <teusjannette@gmail.com>
2019-11-01 07:59:49 +0100

    scrollview macos empty app

---
+++ macos/scrollview/scrollview-macos/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  scrollview-macos
+//
+//  Created by Teus Benschop on 01/11/2019.
+//  Copyright © 2019 Free Software Consultants. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ macos/scrollview/scrollview-macos/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  scrollview-macos
+//
+//  Created by Teus Benschop on 01/11/2019.
+//  Copyright © 2019 Free Software Consultants. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
