afb948b69bd492535601cccfcd303369134f7fc1
Teus Benschop <teusjannette@gmail.com>
2020-12-26 14:32:14 +0100

    Testing receiving focused verse from Accordance
    https://github.com/bibledit/cloud/issues/437

--- macos/accordance/osx/AppDelegate.m
+++ macos/accordance/osx/AppDelegate.m
@@ -17,59 +17,50 @@
 @implementation AppDelegate
 
 
-NSString * searchText = @"";
-
-
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    NSURL *url = [NSURL URLWithString:@"https://bibledit.org:8081"];
-    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
-    [[[self webview] mainFrame] loadRequest:urlRequest];
-    [self.window setContentView:self.webview];
-    [NSEvent addLocalMonitorForEventsMatchingMask:NSEventMaskKeyDown handler:^(NSEvent *incomingEvent) {
-        NSEvent *result = incomingEvent;
-        NSWindow *targetWindowForEvent = incomingEvent.window;
-        if (targetWindowForEvent == self.window) {
-            if (incomingEvent.type == NSEventTypeKeyDown) {
-                NSEventModifierFlags modifier = incomingEvent.modifierFlags;
-                if (modifier & NSEventModifierFlagCommand) {
-                    NSString *characters = incomingEvent.characters;
-                    if ([characters isEqualToString:@"f"]) {
-                        NSAlert *alert = [[NSAlert alloc] init];
-                        alert.messageText = @"Search";
-                        [alert addButtonWithTitle:@"OK"];
-                        [alert addButtonWithTitle:@"Cancel"];
-                        alert.informativeText = [NSString stringWithFormat:@"Search for"];
-                        NSTextField *input = [[NSTextField alloc] initWithFrame:NSMakeRect(0, 0, 200, 24)];
-                        [input setStringValue:searchText];
-                        [alert setAccessoryView:input];
-                        [[alert window] setInitialFirstResponder: input];
-                        NSInteger button = [alert runModal];
-                        if (button == NSAlertFirstButtonReturn) {
-                            searchText = [input stringValue];
-                            if (searchText.length) {
-                                [self.webview searchFor:searchText direction:TRUE caseSensitive:FALSE wrap:TRUE];
-                            } else {
-                                [self.webview setSelectedDOMRange:nil affinity:NSSelectionAffinityDownstream];
-                            }
-                        }
-                        result = nil;
-                    }
-                    if ([characters isEqualToString:@"s"]) {
-                      //[self.send:"2CO 9:2"];
-                      //[self print:nil];
-                      NSString * reference = @"2CO 9:2";
-                      [[NSDistributedNotificationCenter defaultCenter] postNotificationName:@"com.santafemac.scrolledToVerse" object:reference userInfo:nil deliverImmediately:YES];
-                    }
-                }
-            }
+  NSURL *url = [NSURL URLWithString:@"https://bibledit.org:8081"];
+  NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
+  [[[self webview] mainFrame] loadRequest:urlRequest];
+  [self.window setContentView:self.webview];
+  
+  [[NSDistributedNotificationCenter defaultCenter] addObserver:self selector:@selector(accordanceDidScroll:) name:@"com.santafemac.scrolledToVerse" object:nil suspensionBehavior:NSNotificationSuspensionBehaviorCoalesce];
+
+  
+  [NSEvent addLocalMonitorForEventsMatchingMask:NSEventMaskKeyDown handler:^(NSEvent *incomingEvent) {
+    NSEvent *result = incomingEvent;
+    NSWindow *targetWindowForEvent = incomingEvent.window;
+    if (targetWindowForEvent == self.window) {
+      if (incomingEvent.type == NSEventTypeKeyDown) {
+        NSEventModifierFlags modifier = incomingEvent.modifierFlags;
+        if (modifier & NSEventModifierFlagCommand) {
+          NSString *characters = incomingEvent.characters;
+          if ([characters isEqualToString:@"s"]) {
+            NSString * reference = @"2CO 9:2";
+            [[NSDistributedNotificationCenter defaultCenter] postNotificationName:@"com.santafemac.scrolledToVerse" object:reference userInfo:nil deliverImmediately:YES];
+          }
+          if ([characters isEqualToString:@"r"]) {
+          }
         }
-        return result;
-    }];
+      }
+    }
+    return result;
+  }];
 }
 
 
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+  
+  [[NSDistributedNotificationCenter defaultCenter] removeObserver:self name:@"com.santafemac.scrolledToVerse" object:nil];
 
+}
+
+
+-(void)accordanceDidScroll:(NSNotification *)notification {
+  NSLog(@"%@", notification.object);
+  NSString * reference = notification.object;
+  NSLog(reference);
+}
 
 
 @end
