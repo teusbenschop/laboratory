44a670c32023f045b7832ba10a7efa420a193a3d
Teus Benschop <teusjannette@gmail.com>
2022-01-12 20:51:14 +0100

    Lab std::filesystem
    https://github.com/bibledit/cloud/issues/726

---
+++ c++17/main.h
@@ -0,0 +1,9 @@
+
+
+
+
+string filter_url_dirname_internal (string url, const char * separator);
+string filter_url_dirname (string url);
+
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]);
