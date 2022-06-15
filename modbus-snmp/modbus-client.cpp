af0fe7650186a2c04ab7e89ee88e5d2c807e34a5
Teus Benschop <teusjannette@gmail.com>
2022-06-15 17:01:30 +0200

    Stable connection, close sockets

--- modbus-snmp/modbus-client.cpp
+++ modbus-snmp/modbus-client.cpp
@@ -18,23 +18,27 @@ using namespace std;
 
 int main ()
 {
+
+  // Create a new TCP context with proper parameters
+  // (in this example, the local host and the correct port number).
+  modbus_t * ctx = modbus_new_tcp("127.0.0.1", 1502);
+  if (!ctx) {
+    cout << "Failed to create the context: " << modbus_strerror(errno) << endl;
+    return 0;
+  }
+  
+  modbus_set_debug(ctx, TRUE);
+  
+  // Define a timeout of 500ms.
+  modbus_set_response_timeout(ctx, 0, 500000);
+  
   // Keep going.
-  int counter = 0;
   constexpr bool keep_going {true};
   while (keep_going) {
   
-    // Create a new TCP context with proper parameters
-    // (in this example, the local host and the correct port number).
-    modbus_t * ctx = modbus_new_tcp("127.0.0.1", 1502);
-    if (!ctx) {
-      cout << "Failed to create the context: " << modbus_strerror(errno) << endl;
-      return 0;
-    }
-    
-    modbus_set_debug(ctx, TRUE);
-    
-    // Define a timeout of 500ms.
-    modbus_set_response_timeout(ctx, 0, 500000);
+    static int counter = 0;
+    counter++;
+    cout << counter << endl;
     
     // This client connects to the server.
     if (modbus_connect(ctx) == -1) {
@@ -49,30 +53,34 @@ int main ()
     // Some not compliant devices or software uses the slave ID as unit identifier.
     // That's incorrect but without the slave value,
     // the faulty remote device or software drops the requests.
-    modbus_set_slave(ctx, 3);
-    
-    counter++;
-    cout << counter << endl;
+    // modbus_set_slave(ctx, 3);
     
     // This will store the values of the read registers.
     constexpr int number_of_registers { 5 };
-    uint16_t reg [number_of_registers];
-    
-    // Read 5 holding registers starting from address 10.
-    int num = modbus_read_registers (ctx, 10, number_of_registers, reg);
-    // Check the number of read registers whether it's correct.
-    if (num != number_of_registers) {
-      cout << "Failed to read: " << modbus_strerror(errno) << endl;
+    uint16_t registers [number_of_registers];
+
+    // Do several requests then be done with this connection.
+    for (int i = 0; i < 5; i++) {
+
+      // Read 5 holding registers starting from address 10.
+      int num = modbus_read_registers (ctx, 10, number_of_registers, registers);
+      // Check the number of read registers whether it's correct.
+      if (num != number_of_registers) {
+        cout << "Failed to read: " << modbus_strerror(errno) << endl;
+      }
+
+      this_thread::sleep_for(chrono::milliseconds(100));
+
     }
-
-    // Ready.
-    modbus_close(ctx);
-    modbus_free(ctx);
-
-    this_thread::sleep_for(chrono::milliseconds(500));
+    
+    int socket = modbus_get_socket (ctx);
+    if (socket > 0) close (socket);
     
   }
-  
+
+  // Ready.
+  modbus_close(ctx);
+  modbus_free(ctx);
   
   return 0;
 }
--- modbus-snmp/modbus-server.cpp
+++ modbus-snmp/modbus-server.cpp
@@ -47,10 +47,10 @@ int main ()
   // Some not compliant devices or software uses the slave ID as unit identifier.
   // That's incorrect but without the slave value,
   // the faulty remote device or software drops the requests.
-  modbus_set_slave(ctx, 3);
+  // modbus_set_slave(ctx, 3);
 
-  int s = modbus_tcp_listen(ctx, 1);
-  if (s < 0) {
+  int listen_socket = modbus_tcp_listen(ctx, 1);
+  if (listen_socket < 0) {
     cout << "Failed to listen on the socket: " << modbus_strerror(errno) << endl;
     modbus_free(ctx);
     return 0;
@@ -60,37 +60,48 @@ int main ()
   while (keep_going) {
 
     // Block till it accepts a new connection from a client (is master).
-    int accepted_socket = modbus_tcp_accept(ctx, &s);
+    int accepted_socket = modbus_tcp_accept(ctx, &listen_socket);
     if (accepted_socket < 0) {
       cout << "Failed to accept a client on the socket: " << modbus_strerror(errno) << endl;
       continue;
     }
 
     // Buffer to store the request.
-    uint8_t req [MODBUS_TCP_MAX_ADU_LENGTH];
+    uint8_t request [MODBUS_TCP_MAX_ADU_LENGTH];
     
-    // The receive function stores the indication request in the request buffer.
-    // It returns the request length if successful.
-    // The returned request length can be zero if the indication request is ignored
-    // (eg. a query for another slave in RTU mode).
-    // Otherwise it return -1 and set errno.
-    int len = modbus_receive(ctx, req);
-    if (len == 0) continue;
-    if (len < 0) {
-      cout << "Failed to receive the indication request: " << modbus_strerror(errno) << endl;
-      continue;
-    }
+    while (keep_going) {
 
-    // Send the reply based on the mapping.
-    len = modbus_reply(ctx, req, len, mapping);
-    if (len < 0) {
-      cout << "Failed to send the reply to the request: " << modbus_strerror(errno) << endl;
+      // The receive function stores the indication request in the request buffer.
+      // It returns the request length if successful.
+      // The returned request length can be zero if the indication request is ignored
+      // (eg. a query for another slave in RTU mode).
+      // Otherwise it return -1 and set errno.
+      int len = modbus_receive(ctx, request);
+      if (len == 0) continue;
+      if (len < 0) {
+        cout << "Failed to receive the indication request: " << modbus_strerror(errno) << endl;
+        break;
+      }
+      
+      static int counter = 0;
+      counter++;
+      cout << counter << endl;
+
+      // Send the reply based on the mapping.
+      len = modbus_reply(ctx, request, len, mapping);
+      if (len < 0) {
+        cout << "Failed to send the reply to the request: " << modbus_strerror(errno) << endl;
+      }
+      
     }
+    
+    // Close the connection to the client.
+    if (accepted_socket) close (accepted_socket);
   }
 
   // Done: Free the structures.
-  if (s != -1) {
-    close(s);
+  if (listen_socket != -1) {
+    close(listen_socket);
   }
   modbus_mapping_free(mapping);
   modbus_close(ctx);
