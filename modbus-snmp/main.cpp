29b36b7698281d3c55b5806942085f59807f4528
Teus Benschop <teusjannette@gmail.com>
2022-06-15 11:50:38 +0200

    Basic layout modbus

---
+++ modbus-snmp/main.cpp
@@ -0,0 +1,47 @@
+#include <cstdlib>
+#include <unistd.h>
+#include <utime.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <signal.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <cstring>
+#include <algorithm>
+#include <set>
+#include <chrono>
+#include <iomanip>
+#include <stdexcept>
+#include <thread>
+#include <cmath>
+#include <mutex>
+#include <numeric>
+#include <random>
+#include <limits>
+
+
+using namespace std;
+
+
+int main()
+{
+  for (int i = 0; i < 10; ++i) {
+  }
+}
