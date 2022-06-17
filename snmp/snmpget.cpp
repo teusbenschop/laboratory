5cf5df9326effdc3823dd291c925b3f0cb1b4603
Teus Benschop <teusjannette@gmail.com>
2022-06-17 17:15:51 +0200

    Get SNMP values multiple times

--- snmp/snmpget.cpp
+++ snmp/snmpget.cpp
@@ -1,18 +1,6 @@
 #include <string>
 #include <iostream>
 #include <net-snmp/net-snmp-config.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <stdio.h>
-#include <ctype.h>
-# include <sys/time.h>
-# include <time.h>
-#include <sys/select.h>
-#include <netdb.h>
-#include <arpa/inet.h>
 #include <net-snmp/utilities.h>
 #include <net-snmp/net-snmp-includes.h>
 #include <net-snmp/output_api.h>
@@ -33,9 +21,9 @@ static void optProc(int argc, char *const *argv, int opt)
                         netsnmp_ds_toggle_boolean(NETSNMP_DS_APPLICATION_ID, NETSNMP_DS_APP_DONT_FIX_PDUS);
                         break;
                     default:
-                        fprintf(stderr, "Unknown flag passed to -C: %c\n",
-                                optarg[-1]);
-                        exit(1);
+                        string message = string ("Unknown flag passed to -C: ");
+                        message += optarg[-1];
+                        throw message;
                 }
             }
             break;
@@ -46,37 +34,27 @@ static void optProc(int argc, char *const *argv, int opt)
 
 void usage(void)
 {
-    fprintf(stderr, "USAGE: snmpget ");
-    snmp_parse_args_usage(stderr);
-    fprintf(stderr, " OID [OID]...\n\n");
-    snmp_parse_args_descriptions(stderr);
-    fprintf(stderr, " -C APPOPTS\t\tSet various application specific behaviours:\n");
-    fprintf(stderr, "\t\t\t  f:  do not fix errors and retry the request\n");
+    cout << "USAGE: snmpget ";
+    snmp_parse_args_usage(stdout);
+    cout << " OID [OID]..." << endl << endl;
+    snmp_parse_args_descriptions(stdout);
+    cout << " -C APPOPTS Set various application specific behaviours:" << endl;
+    cout << "  f: do not fix errors and retry the request" << endl;
 }
 
 
 int main (int argc, char *argv[])
 {
+    netsnmp_session *ss {nullptr};
+    
     try {
-
-        
-        netsnmp_session session, *ss;
-        netsnmp_pdu    *pdu;
-        netsnmp_pdu    *response;
-        netsnmp_variable_list *vars;
-        int             arg;
-        int             count;
-        int             current_name = 0;
-        char           *names[SNMP_MAX_CMDLINE_OIDS];
-        oid             name[MAX_OID_LEN];
-        size_t          name_length;
-        int             status;
-        int             failures = 0;
-        int             exitval = 1;
         
         SOCK_STARTUP;
-        
+
+        netsnmp_session session {};
+
         // Get the common command line arguments.
+        int arg { 0 };
         switch (arg = snmp_parse_args(argc, argv, &session, "C:", optProc)) {
             case NETSNMP_PARSE_ARGS_ERROR:
                 throw string ("Error parsing arguments");
@@ -100,7 +78,11 @@ int main (int argc, char *argv[])
         }
         
         // Get the object names.
+        char * names[SNMP_MAX_CMDLINE_OIDS];
+        int current_name { 0 };
         for (; arg < argc; arg++) names[current_name++] = argv[arg];
+        
+        int count { 0 };
         
         // Open an SNMP session.
         ss = snmp_open(&session);
@@ -109,15 +91,73 @@ int main (int argc, char *argv[])
             snmp_sess_perror("snmpget", &session);
             throw string ("Cannot open session");
         }
-        
-        
-        
+
+        // Repeat the request several times.
+        for (int repeat = 0; repeat < 5; repeat++) {
+            
+            // Create PDU for GET request and add object names to request.
+            size_t name_length = MAX_OID_LEN;
+            oid name[MAX_OID_LEN];
+            int failures { 0 };
+            netsnmp_pdu * pdu = snmp_pdu_create(SNMP_MSG_GET);
+            for (count = 0; count < current_name; count++) {
+                if (!snmp_parse_oid(names[count], name, &name_length)) {
+                    snmp_perror(names[count]);
+                    failures++;
+                } else
+                    snmp_add_null_var(pdu, name, name_length);
+            }
+            if (failures) throw string ("Cannot parse OID");
+            
+            // Perform the request.
+            netsnmp_pdu *response { nullptr };
+            netsnmp_variable_list *vars { nullptr };
+            int status = snmp_synch_response(ss, pdu, &response);
+            if (status == STAT_SUCCESS) {
+                if (response->errstat == SNMP_ERR_NOERROR) {
+                    for (vars = response->variables; vars; vars = vars->next_variable) {
+                        print_variable(vars->name, vars->name_length, vars);
+                    }
+                }
+                else {
+                    cout << "Error in packet" << endl;
+                    cout << "Reason: " << snmp_errstring(response->errstat) << endl;
+                    
+                    if (response->errindex != 0) {
+                        fprintf(stderr, "Failed object: ");
+                        for (count = 1, vars = response->variables;
+                             vars && count != response->errindex;
+                             vars = vars->next_variable, count++)
+                        /*EMPTY*/;
+                        if (vars) {
+                            fprint_objid(stderr, vars->name, vars->name_length);
+                        }
+                        fprintf(stderr, "\n");
+                    }
+                } // endif -- SNMP_ERR_NOERROR.
+                
+            } // endif -- STAT_SUCCESS.
+            else if (status == STAT_TIMEOUT) {
+                string message = "Timeout: No Response from " + string (session.peername);
+                throw message;
+            }
+            else { // status == STAT_ERROR.
+                snmp_sess_perror("snmpget", ss);
+            }
+            
+            if (response) snmp_free_pdu(response);
+
+            
+        }
         
 
     }
     catch (const string & msg) {
-        cout << msg << endl;
+        cout << "Execution error: " << msg << endl;
     }
+
+    // Close the session.
+    if (ss) snmp_close(ss);
 
     // Cleanup.
     SOCK_CLEANUP;
