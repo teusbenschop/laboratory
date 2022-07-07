37e8ecd452629387074dc3f7c2888b8280315597
Teus Benschop <teusjannette@gmail.com>
2022-07-07 11:06:00 +0200

    Cleanup

--- cpp-core-guidelines/code/main.cpp
+++ cpp-core-guidelines/code/main.cpp
@@ -5,40 +5,12 @@
 #include <ranges>
 #include <chrono>
 #include <thread>
-#include "../gsl/gsl"
 using namespace std;
 #include "main.h"
 
 int main()
 {
-//  double d = 1000;
-//  gsl::Expects (d >= 0);
-//  assert (d);
   
-//  chrono::milliseconds ms = 150ms;
-//  auto ms2 = 500ms;
-//  this_thread::sleep_for (ms + ms2);
-  
-//  vector<int> v = { 1, 2, 3 };
-  
-//  gsl::index i = 0;
-//  while (i < v.size()) {
-//    cout << v[i] << endl;
-//    i++;
-//  }
-
-//  for (const auto& x : v) {
-//    cout << x << endl;
-//  }
-  
-//  for_each(v, [](int x) {
-//    cout << x << endl;
-//  });
-//  for_each(par, v, [](int x) { /* do something with the value of x */ });
-
-//  auto print = [](const auto& n) { cout << ' ' << n; };
-//  ranges::for_each(std::as_const(v), print);
-//  print('\n');
   
   return EXIT_SUCCESS;
 }
