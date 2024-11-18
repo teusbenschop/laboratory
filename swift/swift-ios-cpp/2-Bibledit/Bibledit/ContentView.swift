1ac9307814fd35c8590d61f8716daf869d666879
Teus Benschop <teusjannette@gmail.com>
2024-11-18 19:54:49 +0100

    Advanced and basic views lab setup
    https://github.com/bibledit/cloud/issues/966

--- swift/swift-ios-cpp/2-Bibledit/Bibledit/ContentView.swift
+++ swift/swift-ios-cpp/2-Bibledit/Bibledit/ContentView.swift
@@ -1,37 +1,44 @@
 import SwiftUI
 import WebKit
 
-let webview1 : WebView = WebView()
-let webview2 : WebView = WebView()
-
 let about_blank : String = "about:blank"
+
+let webview_advanced : WebView = WebView()
+let webview_translate : WebView = WebView()
+let webview_resources : WebView = WebView()
+let webview_notes : WebView = WebView()
+let webview_settings : WebView = WebView()
+
 
 struct ContentView: View {
     
-    let timer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
+    let timer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
     
     @State var enable_second_view: Bool = false
     
     var body: some View {
         NavigationStack {
-            webview1
+            webview_advanced
             .navigationDestination(isPresented: $enable_second_view) {
-                webview2
+                BasicView()
                 .navigationBarBackButtonHidden(true)
                 .onAppear() {
-                    print ("webview2 appears")
-                    webview2.loadURL(urlString: "https://freesoftwareconsultants.nl")
-                    webview1.loadURL(urlString: about_blank)
+                    webview_advanced.loadURL(urlString: about_blank)
+                    webview_translate.loadURL(urlString: "https://bibledit.org:8091/editone2/index")
+                    webview_resources.loadURL(urlString: "https://bibledit.org:8091/resource/index")
+                    webview_notes.loadURL(urlString: "https://bibledit.org:8091/notes/index")
+                    webview_settings.loadURL(urlString: "https://bibledit.org:8091/index/index?item=settings")
                 }
             }
             .onAppear() {
-                print ("webview1 appears")
-                webview1.loadURL(urlString: "https://bibledit.org:8091")
-                webview2.loadURL(urlString: about_blank)
+                webview_translate.loadURL(urlString: about_blank)
+                webview_resources.loadURL(urlString: about_blank)
+                webview_notes.loadURL(urlString: about_blank)
+                webview_settings.loadURL(urlString: about_blank)
+                webview_advanced.loadURL(urlString: "https://bibledit.org:8091")
             }
         }
         .onAppear(){
-            print ("body view appears")
         }
         .onReceive(timer) { input in
             enable_second_view.toggle()
@@ -39,7 +46,30 @@ struct ContentView: View {
     }
     
     init() {
-        print ("init")
+    }
+}
+
+
+struct BasicView: View {
+    var body: some View {
+        TabView {
+            webview_translate
+                .tabItem {
+                    Label("Translate", systemImage: "doc")
+                }
+            webview_resources
+                .tabItem {
+                    Label("Resources", systemImage: "book")
+                }
+            webview_notes
+                .tabItem {
+                    Label("Notes", systemImage: "note")
+                }
+            webview_settings
+                .tabItem {
+                    Label("Settings", systemImage: "gear")
+                }
+        }
     }
 }
 
@@ -72,4 +102,3 @@ struct WebView: UIViewRepresentable {
         webView.load(URLRequest(url: URL(string: urlString)!))
     }
 }
-
