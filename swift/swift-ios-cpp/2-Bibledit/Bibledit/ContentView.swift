bdcd0ef18e878bc7edeae9c2c9685eecd7d69571
Teus Benschop <teusjannette@gmail.com>
2024-11-26 21:06:16 +0100

    Improved View switching
    Switch from splash to basic mode to advanced mode, and back again to splash, and so on.
    Demo app for application in production app.
    https://github.com/bibledit/cloud/issues/966

--- swift/swift-ios-cpp/2-Bibledit/Bibledit/ContentView.swift
+++ swift/swift-ios-cpp/2-Bibledit/Bibledit/ContentView.swift
@@ -9,67 +9,98 @@ let webview_resources : WebView = WebView()
 let webview_notes : WebView = WebView()
 let webview_settings : WebView = WebView()
 
+enum ViewState {
+    case splash
+    case basic
+    case advanced
+}
 
 struct ContentView: View {
     
     let timer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
     
-    @State var enable_second_view: Bool = false
+    @State var view_state : ViewState = ViewState.splash
     
     var body: some View {
-        NavigationStack {
-            webview_advanced
-            .navigationDestination(isPresented: $enable_second_view) {
-                BasicView()
-                .navigationBarBackButtonHidden(true)
-                .onAppear() {
-                    webview_advanced.loadURL(urlString: about_blank)
-                    webview_translate.loadURL(urlString: "https://bibledit.org:8091/editone2/index")
-                    webview_resources.loadURL(urlString: "https://bibledit.org:8091/resource/index")
-                    webview_notes.loadURL(urlString: "https://bibledit.org:8091/notes/index")
-                    webview_settings.loadURL(urlString: "https://bibledit.org:8091/index/index?item=settings")
+        VStack {
+            if view_state == ViewState.splash {
+                VStack {
+                    Spacer()
+                    Text("Bibledit")
+                        .dynamicTypeSize(.xxxLarge)
+                    Spacer()
+                    ProgressView()
+                        .dynamicTypeSize(.xxxLarge)
+                    Spacer()
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .scaledToFit()
+                    Spacer()
+                }
+                .onAppear(){
                 }
             }
-            .onAppear() {
-                webview_translate.loadURL(urlString: about_blank)
-                webview_resources.loadURL(urlString: about_blank)
-                webview_notes.loadURL(urlString: about_blank)
-                webview_settings.loadURL(urlString: about_blank)
-                webview_advanced.loadURL(urlString: "https://bibledit.org:8091")
+            if view_state == ViewState.basic {
+                TabView {
+                    webview_translate
+                        .tabItem {
+                            Label("Translate", systemImage: "doc")
+                        }
+                        .onAppear() {
+                            webview_translate.loadURL(urlString: "https://bibledit.org:8091/editone2/index")
+                        }
+                    webview_resources
+                        .tabItem {
+                            Label("Resources", systemImage: "book")
+                        }
+                        .onAppear() {
+                            webview_resources.loadURL(urlString: "https://bibledit.org:8091/resource/index")
+                        }
+                    webview_notes
+                        .tabItem {
+                            Label("Notes", systemImage: "note")
+                        }
+                        .onAppear() {
+                            webview_notes.loadURL(urlString: "https://bibledit.org:8091/notes/index")
+                        }
+                    webview_settings
+                        .tabItem {
+                            Label("Settings", systemImage: "gear")
+                        }
+                        .onAppear() {
+                            webview_settings.loadURL(urlString: "https://bibledit.org:8091/index/index?item=settings")
+                        }
+                }
+                .onAppear() {
+                    webview_advanced.loadURL(urlString: about_blank)
+                }
             }
-        }
-        .onAppear(){
+            if view_state == ViewState.advanced {
+                webview_advanced
+                    .onAppear() {
+                        webview_translate.loadURL(urlString: about_blank)
+                        webview_resources.loadURL(urlString: about_blank)
+                        webview_notes.loadURL(urlString: about_blank)
+                        webview_settings.loadURL(urlString: about_blank)
+                        webview_advanced.loadURL(urlString: "https://bibledit.org:8091")
+                    }
+            }
+
         }
         .onReceive(timer) { input in
-            enable_second_view.toggle()
+            if view_state == ViewState.splash {
+                view_state = ViewState.basic
+            }
+            else if view_state == ViewState.basic {
+                view_state = ViewState.advanced
+            }
+            else if view_state == ViewState.advanced {
+                view_state = ViewState.splash
+            }
         }
     }
     
     init() {
-    }
-}
-
-
-struct BasicView: View {
-    var body: some View {
-        TabView {
-            webview_translate
-                .tabItem {
-                    Label("Translate", systemImage: "doc")
-                }
-            webview_resources
-                .tabItem {
-                    Label("Resources", systemImage: "book")
-                }
-            webview_notes
-                .tabItem {
-                    Label("Notes", systemImage: "note")
-                }
-            webview_settings
-                .tabItem {
-                    Label("Settings", systemImage: "gear")
-                }
-        }
     }
 }
 
