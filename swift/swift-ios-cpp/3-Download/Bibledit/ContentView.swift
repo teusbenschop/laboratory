bc6087de67e90d40959c25c18f79432ca3521ec5
Teus Benschop <teusjannette@gmail.com>
2025-01-03 14:56:20 +0100

    Reduce code, test more, still failure to download
    https://github.com/bibledit/cloud/issues/985

--- swift/swift-ios-cpp/3-Download/Bibledit/ContentView.swift
+++ swift/swift-ios-cpp/3-Download/Bibledit/ContentView.swift
@@ -4,48 +4,74 @@ import SwiftUI
 struct ContentView: View {
     let webView = WebView(request: URLRequest(url: URL(string: "https://bibledit.org:8091/exports/Sample/usfm/full")!))
     var body: some View {
-        VStack {
-            webView
-            HStack {
-                Button(action: {
-                    self.webView.goBack()
-                }){
-                    Image(systemName: "arrowtriangle.left.fill")
-                        .font(.title)
-                        .foregroundColor(.blue)
-                        .padding()
-                }
-                Spacer()
-                Button(action: {
-                    self.webView.goHome()
-                }){
-                    Image(systemName: "house.fill")
-                        .font(.title)
-                        .foregroundColor(.blue)
-                        .padding()
-                }
-                Spacer()
-                Button(action: {
-                    self.webView.refresh()
-                }){
-                    Image(systemName: "arrow.clockwise.circle.fill")
-                        .font(.title)
-                        .foregroundColor(.blue)
-                        .padding()
-                }
-                Spacer()
-                Button(action: {
-                    self.webView.goBack()
-                }){
-                    Image(systemName: "arrowtriangle.right.fill")
-                        .font(.title)
-                        .foregroundColor(.blue)
-                        .padding()
-                }
-            }
-        }
+        webView
     }
 }
+
+
+class Coordinator: NSObject, WKNavigationDelegate, WKDownloadDelegate {
+    
+    let parent: WebView
+    
+    fileprivate var downloadDestinationURL: URL?
+    
+    init(parent: WebView) {
+        self.parent = parent
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, preferences: WKWebpagePreferences, decisionHandler: @escaping (WKNavigationActionPolicy, WKWebpagePreferences) -> Void) {
+        if navigationAction.shouldPerformDownload {
+            decisionHandler(.download, preferences)
+            print ("action decision download")
+        } else {
+            decisionHandler(.allow, preferences)
+            print ("action decision allow")
+        }
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        if navigationResponse.canShowMIMEType {
+            decisionHandler(.allow)
+            print ("response decision allow")
+        } else {
+            decisionHandler(.download)
+            print ("response decision download")
+        }
+    }
+    
+    public func destinationUrlForFile(withName name: String) -> URL? {
+        let temporaryDir = NSTemporaryDirectory()
+        let url = URL(fileURLWithPath: temporaryDir)
+            .appendingPathComponent(UUID().uuidString)
+        
+        if ((try? FileManager.default.createDirectory(at: url, withIntermediateDirectories: false)) == nil) {
+            return nil
+        }
+        
+        return url.appendingPathComponent(name)
+    }
+    
+    func download(_ download: WKDownload,
+                  decideDestinationUsing response: URLResponse,
+                  suggestedFilename: String,
+                  completionHandler: @escaping (URL?) -> Void) {
+        print ("decide destination using")
+        completionHandler(URL(string: "/tmp/ios-download"))
+    }
+    
+    func downloadDidFinish(_ download: WKDownload) {
+        print ("download did finish")
+    }
+    
+    public func download(_ download: WKDownload, didFailWithError error: Error, resumeData: Data?) {
+        print ("download error")
+    }
+    
+    public func downloadDidFailed(withError error: Error) {
+         print (error)
+    }
+}
+
 
 struct WebView: UIViewRepresentable {
     let request: URLRequest
@@ -89,47 +115,4 @@ struct WebView: UIViewRepresentable {
         webView.load(request)
     }
 
-    class Coordinator: NSObject, WKNavigationDelegate {
-        let parent: WebView
-        
-        init(parent: WebView) {
-            self.parent = parent
-        }
-
-        func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, preferences: WKWebpagePreferences, decisionHandler: @escaping (WKNavigationActionPolicy, WKWebpagePreferences) -> Void) {
-            if navigationAction.shouldPerformDownload {
-                decisionHandler(.download, preferences)
-                print ("action decision download")
-            } else {
-                decisionHandler(.allow, preferences)
-                print ("action decision allow")
-            }
-        }
-
-        func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
-            if navigationResponse.canShowMIMEType {
-                decisionHandler(.allow)
-                print ("response decision allow")
-            } else {
-                decisionHandler(.download)
-                print ("response decision download")
-            }
-        }
-
-        func download(_ download: WKDownload, decideDestinationUsing
-                      response: URLResponse, suggestedFilename: String,
-                      completionHandler: @escaping (URL?) -> Void) {
-            print ("call completion handler")
-            completionHandler(URL(string: "/tmp/ios-download"))
-        }
-
-        func downloadDidFinish(_ download: WKDownload) {
-            print ("download did finish")
-        }
-        
-        public func download(_ download: WKDownload, didFailWithError error: Error, resumeData: Data?) {
-            print ("download error")
-        }
-        
-    }
 }
