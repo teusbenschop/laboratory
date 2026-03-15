4d4645442bb9ec9dac8d22f1a760e6f1b19dae54
Teus Benschop <teusjannette@gmail.com>
2026-03-15 17:54:58 +0100

    Remove unused code

--- boost/main.h
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <iostream>
-
-int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]);
-
-void reset_boost_timer ();
-void clear_integer ();
-void reset_condition_variable ();
-void heartbeat_cpp ();
