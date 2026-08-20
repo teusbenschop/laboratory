2969d24ed44d574a18d79a4b8f2721c78ce04a50
Teus Benschop <teusjannette@gmail.com>
2026-08-20 07:11:02 +0200

    ncurses++ demo

---
+++ ncurses/c++-api/main.cpp
@@ -0,0 +1,25 @@
+#include <cstdlib>
+#include <iostream>
+#include <ostream>
+#include <cursesapp.h>
+#include <cursesm.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <clocale>
+
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <vector>
+
+
+int main() {
+    std::cout << "lab" << std::endl;
+
+    return EXIT_SUCCESS;
+}
